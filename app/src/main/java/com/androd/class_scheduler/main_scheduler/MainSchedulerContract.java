eb2aa11466ac2433cced0b6f506bfd0c0d6908c4
Andrew <ShinyChrome>
2017-02-08 20:32:39 +1300

    Created MainScheduler classes

---
+++ app/src/main/java/com/androd/class_scheduler/main_scheduler/MainSchedulerContract.java
@@ -0,0 +1,21 @@
+package com.androd.class_scheduler.main_scheduler;
+
+import com.androd.class_scheduler.base.BaseContract;
+
+/**
+ * Created by Andrew on 8/02/2017.
+ */
+
+public interface MainSchedulerContract{
+	interface View extends BaseContract.View{
+
+	}
+
+	interface Navigation extends BaseContract.Navigation{
+
+	}
+
+	interface Presenter extends BaseContract.Presenter{
+
+	}
+}
---
+++ app/src/main/java/com/androd/class_scheduler/main_scheduler/MainSchedulerView.java
@@ -0,0 +1,11 @@
+package com.androd.class_scheduler.main_scheduler;
+
+import com.androd.class_scheduler.base.BaseContract;
+import com.androd.class_scheduler.base.BaseView;
+
+/**
+ * Created by Andrew on 8/02/2017.
+ */
+
+public class MainSchedulerView extends BaseView implements BaseContract.View, BaseContract.Navigation{
+}
